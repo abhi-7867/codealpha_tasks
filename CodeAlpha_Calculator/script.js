b6011a8edd1f065c588f7f0620f17d631ac99916
abhi-7867 <abhisekasinha43@gmail.com>
2026-01-24 11:36:54 +0530

    feat(calc): implement updateDisplay function

--- CodeAlpha_Calculator/script.js
+++ CodeAlpha_Calculator/script.js
@@ -4,3 +4,8 @@
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('button');
 let currentInput = '';
+
+// --- Update calculator screen ---
+function updateDisplay() {
+  display.textContent = currentInput || '0';
+}
